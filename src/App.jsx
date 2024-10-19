c025f5909d68f43a6ac0ed8a78b867cf4bb699dd
adnan <sirgeant10@gmail.com>
2024-10-19 13:30:16 +0600

    Use useEffect correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
 export default function App() {
   const [counter, setCounter] = useState(0);
 
-  // setInterval(() => {
-  //   setCounter((count) => count + 1);
-  // });
-  
+  useEffect(() => {
+    const key = setInterval(() => {
+      setCounter((count) => count + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(key);
+    };
+  }, []);
+
   return (
     <>
       <h1>{counter} seconds have passed</h1>
