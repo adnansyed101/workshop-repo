c5442a4da997ced61e43638c49adbbd7043d9695
adnan <sirgeant10@gmail.com>
2024-10-21 21:57:14 +0600

    Add styling to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
+import profileImg from "../../assets/images/profile.png"
+
 const Header = () => {
   return (
-    <div>
+    <header className="flex justify-between items-center p-4">
       <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
-    </div>
+      <img src={profileImg} alt="Person" />
+    </header>
   );
 };
 
