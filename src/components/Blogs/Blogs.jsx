ea7a9e1f66371ca6d1b60d23c538ae8cb5eb7a50
adnan <sirgeant10@gmail.com>
2024-10-22 09:51:18 +0600

    Show all blogs

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
@@ -12,6 +13,9 @@ const Blogs = () => {
   return (
     <div className="md:w-2/3">
       <h1>Blogs</h1>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog}></Blog>
+      ))}
     </div>
   );
 };
