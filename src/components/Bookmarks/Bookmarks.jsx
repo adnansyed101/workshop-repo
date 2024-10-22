5bcc7ef3b15a844ed6ca5ff518e7d034a4b275e2
adnan <sirgeant10@gmail.com>
2024-10-22 15:10:51 +0600

    Show reading time and fix key warning

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,12 +1,15 @@
 import PropTypes from "prop-types";
 import Bookmark from "../Bookmark/Bookmark";
 
-const Bookmarks = ({ bookmarks }) => {
+const Bookmarks = ({ bookmarks, readingTime }) => {
   return (
     <div className="md:w-1/3 bg-gray-200 ml-4 mt-4 p-4">
+      <div>
+        <h3 className="text-4xl">Reading Time: {readingTime}</h3>
+      </div>
       <h1 className="text-3xl text-center">Bookmarks: {bookmarks.length}</h1>
-      {bookmarks.map((bookmark) => (
-        <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>
+      {bookmarks.map((bookmark, index) => (
+        <Bookmark key={index} bookmark={bookmark}></Bookmark>
       ))}
     </div>
   );
@@ -14,6 +17,7 @@ const Bookmarks = ({ bookmarks }) => {
 
 Bookmarks.propTypes = {
   bookmarks: PropTypes.array.isRequired,
+  readingTime: PropTypes.number
 };
 
 export default Bookmarks;
