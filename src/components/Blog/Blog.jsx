322a22201a798bf0ad775c240f95cffe99598112
adnan <sirgeant10@gmail.com>
2024-10-22 10:00:50 +0600

    Add bookmark icon

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { CiBookmark } from "react-icons/ci";
 
 const Blog = ({ blog }) => {
   const {
@@ -11,10 +12,10 @@ const Blog = ({ blog }) => {
     hashtags,
   } = blog;
   return (
-    <div>
-      <img src={cover} alt={`Cover Picture of the title ${title}`} />
+    <div className="mb-20">
+      <img src={cover} alt={`Cover Picture of the title ${title}`} className="w-full mb-8"/>
       <div className="flex justify-between">
-        <div>
+        <div className="flex mb-4">
           <img className="w-14" src={author_img} alt="Author Image" />
           <div className="ml-6">
             <h3 className="text-2x">{author}</h3>
@@ -23,6 +24,7 @@ const Blog = ({ blog }) => {
         </div>
         <div>
           <span>{reading_time} mid read</span>
+          <button className="ml-2 text-2xl text-red-600"><CiBookmark /></button>
         </div>
       </div>
       <h2 className="text-4xl">{title}</h2>
