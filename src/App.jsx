0f667b51d130f02bf1e9a87ae7af13cfecdf9f4f
adnan <sirgeant10@gmail.com>
2024-10-22 10:06:06 +0600

    Add bookmark state and handlebookmark function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import Blogs from "./components/Blogs/Blogs";
 import Bookmarks from "./components/Bookmarks/Bookmarks";
 import Header from "./components/Header/Header";
 
 const App = () => {
+  const [bookmarks, setBookmarks] = useState([]);
+
+  const handleAddToBookmark = () => {
+    console.log("Adding to bookmark");
+  };
+
   return (
     <>
       <Header></Header>
       <main className="md:flex max-w-7xl mx-auto">
-        <Blogs></Blogs>
+        <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs>
         <Bookmarks></Bookmarks>
       </main>
     </>
