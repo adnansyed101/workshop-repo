6623cd4b64b6225f38a3ce645612b32bf715cad3
adnan <sirgeant10@gmail.com>
2024-10-28 11:09:52 +0600

    Clean up for new practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,8 @@
-import { useState } from "react";
-import Blogs from "./components/Blogs/Blogs";
-import Bookmarks from "./components/Bookmarks/Bookmarks";
-import Header from "./components/Header/Header";
-
 const App = () => {
-  const [bookmarks, setBookmarks] = useState([]);
-  const [readingTime, setReadingTime] = useState(0);
-
-  const handleAddToBookmark = (blog) => {
-    setBookmarks((prev) => [...prev, blog]);
-  };
-
-  const handleMarkAsRead = (time, id) => {
-    setReadingTime((prev) => prev + time);
-
-    const remainingBookMarks = bookmarks.filter(
-      (bookmark) => bookmark.id !== id
-    );
-
-    setBookmarks(remainingBookMarks);
-  };
-
   return (
-    <>
-      <Header></Header>
-      <main className="md:flex max-w-7xl mx-auto">
-        <Blogs
-          handleAddToBookmark={handleAddToBookmark}
-          handleMarkAsRead={handleMarkAsRead}
-        ></Blogs>
-        <Bookmarks bookmarks={bookmarks} readingTime={readingTime}></Bookmarks>
-      </main>
-    </>
+    <div>
+      <h1>Hello world</h1>
+    </div>
   );
 };
 
