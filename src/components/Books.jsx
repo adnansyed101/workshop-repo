e8eec69b1c9d0f22a8159c9ff5ef1aa04dd40780
adnan <sirgeant10@gmail.com>
2024-10-31 22:19:08 +0600

    Create Books comp and show book comp

---
+++ src/components/Books.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import Book from "./Book";
+
+const Books = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("./booksData.json")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
+
+  return (
+    <div>
+      <h2 className="text-4xl font-bold text-center">Books</h2>
+      <div>
+        {books.map((book) => (
+          <Book key={book.bookId} book={book}></Book>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Books;
