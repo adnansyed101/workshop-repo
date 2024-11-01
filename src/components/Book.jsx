feac00af9b1a472ac52f2ce34dbaf1d299c95d5e
adnan <sirgeant10@gmail.com>
2024-11-01 14:21:48 +0600

    Show card tag and divider

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 
 const Book = ({ book }) => {
-  const { image, bookName, author } = book;
+  const { image, bookName, author, tags } = book;
 
   return (
     <div className="card bg-base-100 w-96 shadow-xl p-6">
@@ -9,9 +9,20 @@ const Book = ({ book }) => {
         <img src={image} alt="Book" className="h-[166px]" />
       </figure>
       <div className="card-body">
+        <div className="flex justify-center gap-4">
+          {tags.map((tag) => (
+            <button
+              key={tag}
+              className="btn btn-xs bg-green-300 text-green-500"
+            >
+              {tag}
+            </button>
+          ))}
+        </div>
         <h2 className="card-title">{bookName}</h2>
         <div className="badge badge-secondary">NEW</div>
         <p>By: {author}</p>
+        <div className="border-t-2 border-dashed"></div>
         <div className="card-actions justify-end">
           <div className="badge badge-outline">Fashion</div>
           <div className="badge badge-outline">Products</div>
