846f0d63ba9c94b379868cb95ab45ff5b3b3546d
adnan <sirgeant10@gmail.com>
2024-11-09 14:27:22 +0600

    Clean up directory

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App></App>
+    <RouterProvider router={router} />
   </StrictMode>
 );
