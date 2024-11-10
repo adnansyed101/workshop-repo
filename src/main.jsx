c526e6d7b1d8fde7f9d9f4b596708110871f745e
adnan <sirgeant10@gmail.com>
2024-11-10 21:44:26 +0600

    Add routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,29 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
+import Root from "./components/Root";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App></App>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+      {
+        path: "register",
+        element: <Register></Register>,
+      },
+    ],
   },
 ]);
 
