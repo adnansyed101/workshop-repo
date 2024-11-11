ca68a4bd4b7242da2c5518370d9860910c479137
adnan <sirgeant10@gmail.com>
2024-11-11 14:05:03 +0600

    Set createeuser and sign in user

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,11 +1,26 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import PropTypes from "prop-types";
 import { createContext } from "react";
+import auth from "../firebase.init";
 
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
   const authInfo = {
     name: "Adnan",
+    createUser,
+    signInUser,
   };
 
   return (
