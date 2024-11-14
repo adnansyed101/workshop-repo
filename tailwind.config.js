dd2b0d50c16de39bc9f9c7c67d4ff031e3faa8bf
adnan <sirgeant10@gmail.com>
2024-11-14 17:28:48 +0600

    Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
-import daisyui from "daisyui";
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
