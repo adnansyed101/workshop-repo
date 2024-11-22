4dbc70b0a0276f1b31e1a38a06ac892f1ad5b1d0
adnan <sirgeant10@gmail.com>
2024-11-22 21:28:06 +0600

    Add cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const phones = require("./phones.json");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
