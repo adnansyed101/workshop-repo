9c4b90f6f4a3804a6af4c30358c924ab7179f45e
adnan <sirgeant10@gmail.com>
2024-11-22 20:53:44 +0600

    Show all phones and single phone data

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-import express from "express";
+const express = require("express");
+const phones = require("./phones.json");
+
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/phones", (req, res) => {
+  res.send(phones);
+});
+
+app.get("/phones/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id);
+
+  const phone = phones.find((phone) => phone.id === id) || {};
+
+  res.send(phone);
 });
 
 app.listen(PORT, () => {
