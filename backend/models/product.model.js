7ce0f109efdcc6460cfd258be71c1f95efeb2435
adnan <sirgeant10@gmail.com>
2024-11-29 18:26:32 +0600

    Create product model

---
+++ backend/models/product.model.js
@@ -0,0 +1,24 @@
+import mongoose, { mongo } from "mongoose";
+import { type } from "os";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
