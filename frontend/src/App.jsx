012d883a13d5277aab2c0dfb54ec01b063e07f4b
adnan <sirgeant10@gmail.com>
2024-11-29 22:09:57 +0600

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { Box } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import CreatePage from "./pages/CreatePage";
+
+function App() {
+  return (
+    <Box minH={"100vh"}>
+      {/* Navbar */}
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
+  );
+}
+
+export default App;
