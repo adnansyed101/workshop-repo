e4d9a72c933abd3414ef3f7163e223cd75662ef6
adnan <sirgeant10@gmail.com>
2024-11-30 15:12:16 +0600

    Add background color

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useColorModeValue } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Box minH={"100vh"}>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
       {/* Navbar */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/create" element={<CreatePage />} />
