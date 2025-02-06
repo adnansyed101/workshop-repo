ed443c189b1bbe49748af452d79358f71079cc99
Adnan <sirgeant10@gmail.com>
2025-02-06 18:52:31 +0600

    Add box animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { motion } from "motion/react";
+
 const App = () => {
-  return <div>
-    <div className="box"></div>
-  </div>;
+  return (
+    <div>
+      <motion.div
+        className="box"
+        animate={{
+          x: 100,
+        }}
+        transition={{
+          duration: 3,
+          delay: 1,
+        }}
+      ></motion.div>
+      <motion.div className="circle"></motion.div>
+    </div>
+  );
 };
 
 export default App;
