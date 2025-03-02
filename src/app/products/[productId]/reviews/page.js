82c9136e34f8529dac80895809f61602b7057924
Adnan <sirgeant10@gmail.com>
2025-03-03 04:51:59 +0600

    Create review list page.

---
+++ src/app/products/[productId]/reviews/page.js
@@ -0,0 +1,13 @@
+export default function ReviewList() {
+  return (
+    <>
+      <h1>Review List</h1>
+      <ul>
+        <li>Review 1</li>
+        <li>Review 2</li>
+        <li>Review 3</li>
+        <li>Review 4</li>
+      </ul>
+    </>
+  );
+}
