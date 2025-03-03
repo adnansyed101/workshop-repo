deac47e60cab59e4bc68f00bdf769734ee57559d
Adnan <sirgeant10@gmail.com>
2025-03-03 16:04:49 +0600

    Show 404 page.

--- src/app/products/[productId]/reviews/[reviewId]/page.js
+++ src/app/products/[productId]/reviews/[reviewId]/page.js
@@ -1,5 +1,11 @@
+import { notFound } from "next/navigation";
+
 export default async function ProductReview({ params }) {
   const { productId, reviewId } = await params;
+
+  if (parseInt(reviewId) > 1000) {
+    notFound();
+  }
 
   return (
     <h1>
