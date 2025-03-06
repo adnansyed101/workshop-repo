00213c2d077a92cf5b20d523d8e4999b0d819ad5
Adnan <sirgeant10@gmail.com>
2025-03-06 19:51:09 +0600

    Add params and query params.

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+function Home() {
+  return (
+    <>
+      <h1>Welcome Home!</h1>
+      <Link href={"/blog"}>Blog</Link>
+      <Link href={"/products"}>Products</Link>
+      <Link href={"/articles/breaking-news-123?lang=en"}>Read in English</Link>
+      <Link href={"/articles/breaking-news-123?lang=fr"}>Read in French</Link>
+    </>
+  );
+}
+
+export default Home;
