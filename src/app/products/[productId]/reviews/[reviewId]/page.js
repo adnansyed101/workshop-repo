a6437570969708e1ac2996678ee80d2c18d16203
Adnan <sirgeant10@gmail.com>
2025-03-13 14:36:16 +0600

    Throw error.

--- src/app/products/[productId]/reviews/[reviewId]/page.js
+++ src/app/products/[productId]/reviews/[reviewId]/page.js
@@ -1,7 +1,16 @@
 import { notFound, redirect } from "next/navigation";
 
+function getRandomInt(count) {
+  return Math.floor(Math.random() * count);
+}
+
 export default async function ProductReview({ params }) {
   const { productId, reviewId } = await params;
+  const random = getRandomInt(2);
+
+  if (random === 1) {
+    throw Error("Error Loading Review");
+  }
 
   if (parseInt(reviewId) > 1000) {
     // notFound();
