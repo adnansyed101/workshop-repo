302d35e4833fa8c8ce4526c3404c07ab8c372607
adnan <sirgeant10@gmail.com>
2025-03-14 22:03:29 +0600

    Add login to layout.

--- src/app/complex-dashboard/layout.js
+++ src/app/complex-dashboard/layout.js
@@ -3,8 +3,11 @@ export default function ComplexDashboard({
   users,
   revenue,
   notifications,
+  login,
 }) {
-  return (
+  const isLoggedIn = true;
+
+  return isLoggedIn ? (
     <div>
       <div>{children}</div>
       <div style={{ display: "flex" }}>
@@ -15,5 +18,7 @@ export default function ComplexDashboard({
         <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
       </div>
     </div>
+  ) : (
+    login
   );
 }
