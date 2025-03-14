9462b13a13da4949a2e9244b14a5e43ee058b311
adnan <sirgeant10@gmail.com>
2025-03-14 21:35:32 +0600

    Add styling to layout.

--- src/app/complex-dashboard/layout.js
+++ src/app/complex-dashboard/layout.js
@@ -1,7 +1,19 @@
-export default function ComplexDashboard({ chilren }) {
+export default function ComplexDashboard({
+  children,
+  users,
+  revenue,
+  notifications,
+}) {
   return (
-    <>
+    <div>
       <div>{children}</div>
-    </>
+      <div style={{ display: "flex" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div>{users}</div>
+          <div>{revenue}</div>
+        </div>
+        <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
+      </div>
+    </div>
   );
 }
