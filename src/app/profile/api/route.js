edd8e98323b442cfb43183418fb07740a3df155b
adnan <sirgeant10@gmail.com>
2025-03-15 15:41:39 +0600

    Show headers in log.

--- src/app/profile/api/route.js
+++ src/app/profile/api/route.js
@@ -1,3 +1,12 @@
-export async function GET() {
-  return new Response("Hello world!");
+import { headers } from "next/headers";
+
+export async function GET(request) {
+  // const requestHeaders = new Headers(request.headers);
+  // console.log(requestHeaders.get("Authorization"));
+  const headersList = await headers();
+  console.log(headersList.get("Authorization"));
+
+  return new Response("<h1>Profile API Data</h1>", {
+    headers: { "Content-Type": "text/html" },
+  });
 }
