95135bd960335e82bcb6d93da57ef8566fa754b6
adnan <sirgeant10@gmail.com>
2025-03-15 15:07:34 +0600

    Add patch request for comments.

--- src/app/comments/[id]/route.js
+++ src/app/comments/[id]/route.js
@@ -6,3 +6,14 @@ export async function GET(_request, { params }) {
 
   return Response.json(comment);
 }
+
+export async function PATCH(request, { params }) {
+  const { id } = await params;
+  const body = await request.json();
+  const { text } = body;
+
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
+  comments[index].text = text;
+
+  return Response.json(comments[index]);
+}
