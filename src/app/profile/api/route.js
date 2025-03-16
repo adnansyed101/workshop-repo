e04953b136ed55a296965a7a27a300beb8697c3b
adnan <sirgeant10@gmail.com>
2025-03-16 12:22:48 +0600

    Show cookies.

--- src/app/profile/api/route.js
+++ src/app/profile/api/route.js
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request) {
   // const requestHeaders = new Headers(request.headers);
@@ -6,7 +6,15 @@ export async function GET(request) {
   const headersList = await headers();
   console.log(headersList.get("Authorization"));
 
+  const theme = request.cookies.get("theme");
+  console.log(theme);
+
+  const cookieStore = await cookies();
+  cookieStore.set("resultsPerPage", "20");
+
+  console.log(cookieStore.get("resultsPerPage"));
+
   return new Response("<h1>Profile API Data</h1>", {
-    headers: { "Content-Type": "text/html" },
+    headers: { "Content-Type": "text/html", "Set-Cookie": "theme=dark" },
   });
 }
